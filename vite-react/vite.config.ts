60565c101469b76535a7d8703bccf32163862baf
JongWoo Ha <jongwooha98@gmail.com>
2024-03-27 16:22:24 +0900

    feat(wip): Expose button component with `vite-plugin-federation`

--- vite-react/vite.config.ts
+++ vite-react/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), federation({
+    name: 'vite-react',
+    filename: 'remoteEntry.js',
+    exposes: {
+      './Button': './src/shared/Button.tsx',
+    },
+    // remotes:{
+    //     foo: 'remote_foo'
+    // },
+    shared: ['react', 'react-dom']
+  })],
 })
+
+
